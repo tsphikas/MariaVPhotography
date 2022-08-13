8bf1b4e7d6c09672ecd860441a2a6010c4a1fd9a
Theodoros Sphikas <TSphikas@gmail.com>
2022-08-13 19:28:50 +0100

    Basic Design Complete

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,7 +1,55 @@
 let index = 0;
 const totalWorkItems = $(".work-item").length;
 
+$(window).on("load", function () {
+  $(".preloader").addClass("loaded");
+});
+
 $(document).ready(function () {
+  // nav toggle
+  $(".nav-toggle").click(function () {
+    $(".header .nav").slideToggle();
+  });
+  $(".header .nav a").click(function () {
+    if ($(window).width() < 768) {
+      $(".header .nav").slideToggle();
+    }
+  });
+
+  // Fixed header
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".header").addClass("fixed");
+    } else {
+      $(".header").removeClass("fixed");
+    }
+  });
+
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+
   let viewPortHeight = window.innerHeight;
 
   $(".lightbox-img").css("max-height", viewPortHeight * 0.9 + "px");
